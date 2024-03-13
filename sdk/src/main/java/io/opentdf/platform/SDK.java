2328fd2bec21fb6060beca2b1bac34550eadca4e
Tim Tschampel <timothy.tschampel@gmail.com>
2024-03-13 11:11:27 -0700

    feat(codegen): Generate and publish Java Proto generated artifacts (#2)
    * initial addition
    
    * fix workflow
    
    * add sdk stub
    
    * exclude idp plugin

---
+++ sdk/src/main/java/io/opentdf/platform/SDK.java
@@ -0,0 +1,4 @@
+package io.opentdf.platform;
+
+public class SDK {
+}
---
+++ sdk/src/test/java/io/opentdf/platform/SDKTest.java
@@ -0,0 +1,12 @@
+package io.opentdf.platform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SDKTest {
+
+
+    @Test
+    public void testIt(){}
+}
