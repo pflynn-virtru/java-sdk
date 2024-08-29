3aced86cd9d3b23f4ed9a3be4568637681673acb
Morgan Kleene <mkleene@virtru.com>
2024-08-29 15:56:39 -0400

    chore(sdk): add some debug logging (#150)

--- sdk/src/main/java/io/opentdf/platform/sdk/KASKeyCache.java
+++ sdk/src/main/java/io/opentdf/platform/sdk/KASKeyCache.java
@@ -1,11 +1,15 @@
 package io.opentdf.platform.sdk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
 public class KASKeyCache {
+    private static final Logger log = LoggerFactory.getLogger(KASKeyCache.class);
     Map<KASKeyRequest, TimeStampedKASInfo> cache;
 
     public KASKeyCache() {
@@ -17,24 +21,30 @@ public class KASKeyCache {
     }
 
     public Config.KASInfo get(String url, String algorithm) {
+        log.debug("retrieving kasinfo for url = [{}], algorithm = [{}]", url, algorithm);
         KASKeyRequest cacheKey = new KASKeyRequest(url, algorithm);
         LocalDateTime now = LocalDateTime.now();
         TimeStampedKASInfo cachedValue = cache.get(cacheKey);
 
         if (cachedValue == null) {
+            log.debug("didn't find kasinfo for url = [{}], algorithm = [{}]", url, algorithm);
             return null;
         }
 
         LocalDateTime aMinAgo = now.minus(5, ChronoUnit.MINUTES);
         if (aMinAgo.isAfter(cachedValue.timestamp)) {
+            log.debug("cached value is too old timestamp = [{}] for url = [{}], algorithm = [{}]",
+                    cachedValue.timestamp, url, algorithm);
             cache.remove(cacheKey);
             return null;
         }
 
+        log.debug("successfully returned kasInfo = [{}], url = [{}], algorithm = [{}]", cachedValue.kasInfo, url, algorithm);
         return cachedValue.kasInfo;
     }
 
     public void store(Config.KASInfo kasInfo) {
+        log.debug("storing kasInfo into the cache {}", kasInfo);
         KASKeyRequest cacheKey = new KASKeyRequest(kasInfo.URL, kasInfo.Algorithm);
         cache.put(cacheKey, new TimeStampedKASInfo(kasInfo, LocalDateTime.now()));
     }
